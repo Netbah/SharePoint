5739e39be167bedfc64cc6f41ff1b883dc600bcc
netbah <sergey.bakhtoyarov@gmail.com>
2017-04-17 21:00:39 +0200

    Add Solution

---
+++ SharePoint 2016/Lists/DragAndDropList/DragAndDropLIst/Features/DragAndDropListFeature/DragAndDropListFeature.feature
@@ -0,0 +1,11 @@
+﻿<?xml version="1.0" encoding="utf-8"?>
+<feature xmlns:dm0="http://schemas.microsoft.com/VisualStudio/2008/DslTools/Core" dslVersion="1.0.0.0" Id="fe73c5dd-b665-46b0-9e86-0108eeb0e92e" defaultResourceFile="Resources.en.resx" description="&quot;$Resources:DragAndDropListFeature_Description&quot;" featureId="fe73c5dd-b665-46b0-9e86-0108eeb0e92e" imageUrl="" solutionId="00000000-0000-0000-0000-000000000000" title="&quot;$Resources:DragAndDropListFeature_Title&quot;" version="" deploymentPath="$SharePoint.Project.FileNameWithoutExtension$_$SharePoint.Feature.FileNameWithoutExtension$" xmlns="http://schemas.microsoft.com/VisualStudio/2008/SharePointTools/FeatureModel">
+  <projectItems>
+    <projectItemReference itemId="a58d0a56-6c54-4c12-82ac-abcc87f5525e" />
+    <projectItemReference itemId="aa866b75-6660-475f-a006-a98446119e4e" />
+    <projectItemReference itemId="28f77990-f2d3-4b70-b71b-b59dab76fcae" />
+    <projectItemReference itemId="19f0657d-5bee-40c6-ae60-4143f6904569" />
+    <projectItemReference itemId="98346e1c-0a5e-4818-81f0-b95b2da8cd04" />
+    <projectItemReference itemId="35910d68-8f20-4276-b69b-2c157bd26516" />
+  </projectItems>
+</feature>
---
+++ SharePoint 2016/Lists/DragAndDropList/DragAndDropLIst/pkg/Debug/Drag And Drop List/Layouts/DragAndDropList/listView.js
@@ -0,0 +1,131 @@
+﻿(function () {
+
+    SPClientTemplates.TemplateManager.RegisterTemplateOverrides({
+
+        OnPostRender: function (ctx) {
+            var GragAndDropList = {
+                data: ctx.ListData.Row.map(function (x) {
+                    return {
+                        ID: x.ID,
+                        ElementOrderNumber: arguments[1]
+                    };
+                }),
+                listTitle: ctx.ListTitle,
+                table: null
+            };
+
+
+            var startOrderNumber,
+			rows,
+			rowElementId,
+			tr,
+			tbody;
+
+            //Get Table and set it sorteble with JQuery UI
+            rows = ctx.ListData.Row;
+            rowElementId = GenerateIIDForListItem(ctx, rows[0]);
+            tr = document.getElementById(rowElementId);
+            tbody = tr.parentElement;
+            GragAndDropList.table = $(tbody).sortable({
+                stop: function (event, ui) {
+                    var newOrderNumer = ui.item.index();
+                    updateData(GragAndDropList.data, startOrderNumber, newOrderNumer);
+                },
+                start: function (event, ui) {
+                    startOrderNumber = ui.item.index();
+                }
+            });
+
+            //Hide view header   
+            $(tbody.parentElement.parentElement.firstElementChild).hide();
+
+            //Save changes
+            function updateData(data, start, finish) {
+                var d = data.slice(0);
+                d.sort(function (a, b) {
+                    return a.ElementOrderNumber - b.ElementOrderNumber
+                });
+
+                var dragged = d[start],
+				a = start,
+				b = finish,
+				c = -1;
+                var goUp = (start - finish) > 0;
+                if (goUp) {
+                    a = finish;
+                    b = start;
+                    c = 1
+                }
+
+                for (var i = 0; i <= d.length; i++) {
+                    saveState = d[i];
+                    if ((i > a) && (i < b)) {
+                        d[i].ElementOrderNumber = d[i].ElementOrderNumber + 1 * c;
+                        updateItem(d[i], success, failure);
+                    } else if (!goUp && (i == b)) {
+                        d[i].ElementOrderNumber--;
+                        updateItem(d[i], success, failure);
+                    } else if (goUp && (i == a)) {
+                        d[i].ElementOrderNumber++;
+                        updateItem(d[i], success, failure);
+                    } else if (i > finish) {
+                        dragged.ElementOrderNumber = finish;
+                        updateItem(dragged, success, failure);
+                        break;
+                    }
+                }
+
+                function success() {
+                    data = d;
+                }
+
+                function failure() {
+                    GragAndDropList.table.sortable("cancel");
+                }
+
+            }
+
+            function getItemTypeForListName(name) {
+                return "SP.Data." + name.charAt(0).toUpperCase() + name.slice(1) + "ListItem";
+            }
+
+            function updateItem(item, success, failure) {
+                var listItemId = item.ID;
+                var itemProperties = {
+                    ElementOrderNumber: item.ElementOrderNumber
+                }
+                var webUrl = _spPageContextInfo.webAbsoluteUrl
+                var listItemUri = webUrl + "/_api/web/lists/getbytitle('" + GragAndDropList.listTitle + "')/items(" + listItemId + ")";
+                var itemPayload = {
+                    '__metadata': {
+                        'type': getItemTypeForListName(GragAndDropList.listTitle)
+                    }
+                };
+                for (var prop in itemProperties) {
+                    itemPayload[prop] = itemProperties[prop];
+                }
+                updateJson(listItemUri, itemPayload, success, failure);
+            }
+
+            function updateJson(endpointUri, payload, success, error) {
+                $.ajax({
+                    url: endpointUri,
+                    type: "POST",
+                    data: JSON.stringify(payload),
+                    contentType: "application/json;odata=verbose",
+                    headers: {
+                        "Accept": "application/json;odata=verbose",
+                        "X-RequestDigest": $("#__REQUESTDIGEST").val(),
+                        "X-HTTP-Method": "MERGE",
+                        "If-Match": "*"
+                    },
+                    success: success,
+                    error: error
+                });
+            }
+
+        }
+
+    });
+
+})();
